532a1bc27f05bd2a957bef8011f9c1833cb1f9ba
smondeli <sreekanth.mondeli@oracle.com>
2021-10-27 00:19:05 +0530

    Empty project with web, jpa, h2, lombok dependencies

---
+++ src/main/java/com/omnirio/catalog/OmnirioCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.omnirio.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmnirioCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmnirioCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omnirio/catalog/OmnirioCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omnirio.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmnirioCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
